e000e60f8e3db03fb6101b019556a5c8b026bf1c
Mezbaul Haque <mezba109@gmail.com>
2023-03-07 19:01:27 +0600

    Fix example.sh script

--- example.sh
+++ example.sh
@@ -8,11 +8,11 @@ run_without_docker() {
   python -m pip install --upgrade pip
   python -m pip install --requirement requirements.txt
 
-  # Start the worker in the background.
+  # Start the worker.
   python manage.py run_tsp_solver &
 
   # Start the API server.
-  python manage.py runserver 8000
+  python manage.py runserver 8000 &
 }
 
 if [ "$1" = "docker" ]; then
@@ -20,3 +20,9 @@ if [ "$1" = "docker" ]; then
 else
   run_without_docker
 fi
+
+# Kill spawned process group.
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
+# Wait for the processes to complete.
+wait
